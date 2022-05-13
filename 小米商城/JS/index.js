3ddb3bbba55ec63cd0a7001e83b54df7830263d0
“Waylon06” <205218290@qq.com>
2022-05-14 00:00:11 +0800

    22.5.14.1

---
+++ 小米商城/JS/index.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function () {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
+})
