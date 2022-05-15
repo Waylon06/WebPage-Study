2143cc391f42e171ba642cccbfa7874bb0cdc15c
“Waylon06” <205218290@qq.com>
2022-05-15 21:30:26 +0800

    22.5.15.1

--- 小米商城/JS/index.js
+++ 小米商城/JS/index.js
@@ -12,5 +12,23 @@ window.addEventListener('load', function () {
       prevEl: ".swiper-button-prev",
     },
   });
-  
+  // 侧边导航栏
+  var sideNav = document.querySelector('.side-nav').querySelector('ul').querySelectorAll('li');
+  for (let i = 0; i < sideNav.length; i++) {
+    sideNav[i].addEventListener('mouseenter', function() {
+      for (let k = 0; k < sideNav.length; k++) {
+        sideNav[k].style.backgroundColor = '';
+      }
+      sideNav[i].style.backgroundColor = '#ff6700';
+    });
+  }
+  for (let i = 0; i < sideNav.length; i++) {
+    sideNav[i].addEventListener('mouseleave', function() {
+      for (let k = 0; k < sideNav.length; k++) {
+        sideNav[k].style.backgroundColor = '';
+        
+      }
+    })
+    
+  }
 })
