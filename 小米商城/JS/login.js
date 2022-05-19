aff709f55203a6bc8256f75f0fc6f381845de5fa
“Waylon06” <205218290@qq.com>
2022-05-19 23:59:08 +0800

    22.5.19.2

--- 小米商城/JS/login.js
+++ 小米商城/JS/login.js
@@ -3,26 +3,39 @@ window.addEventListener('load', function () {
     var btn = document.querySelector('.btn');
     var un = document.querySelector('#username');
     var psw = document.querySelector('#password');
+    var checked = this.document.querySelector('.ck');
+
     btn.addEventListener('click', function () {
-        const connect = new XMLHttpRequest();
-        connect.onreadystatechange = function () {
-            if (connect.readyState === 4 && connect.status === 200) {
-                // console.log(connect.response.data);
-                console.log(JSON.parse(connect.responseText));
-                window.location.href = "../index.html";
-            } else {
-                console.log("请求失败:" + connect.status);
+        if (checked.checked === true) {
+            // console.log(checked.checked);++++++x
+            const connect = new XMLHttpRequest();
+
+            connect.open('post', 'http://43.138.138.11:1110/api/users/login');
+            connect.setRequestHeader('Content-Type', 'application/json');
+            const PARAMS = {
+                username: un.value,
+                password: psw.value
             }
+            console.log(PARAMS.username);
+            console.log(PARAMS.password);
+            connect.send(JSON.stringify(PARAMS));
+
+            connect.onreadystatechange = function () {
+                if (connect.readyState === 4 && connect.status === 200) {
+                    const resp = JSON.parse(connect.responseText)
+                    if (resp.code == 200) {
+                        console.log(connect.response.data);
+                        console.log(JSON.parse(connect.responseText));
+                        window.location.href = "../index.html";
+                    }
+                } else {
+                    console.log("请求失败:" + connect.status);
+                }
+            }
+
+        } else {
+            console.log(checked.checked);
         }
-        connect.open('post', 'http://43.138.138.11:1110/api/users/login');
-        connect.setRequestHeader('Content-Type', 'application/json');
-        const PARAMS = {
-            username: un.value,
-            password: psw.value
-        }
-        console.log(PARAMS.username);
-        console.log(PARAMS.password);
-        connect.send(JSON.stringify(PARAMS));
     });
-    
+
 })
