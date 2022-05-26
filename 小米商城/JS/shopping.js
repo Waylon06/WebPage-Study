948af81b4b2d24ba3538dfa1acd90ebd2676dace
“Waylon06” <205218290@qq.com>
2022-05-26 23:09:38 +0800

    22.5.26.3

--- 小米商城/JS/shopping.js
+++ 小米商城/JS/shopping.js
@@ -2,6 +2,17 @@ window.addEventListener("load", function () {
     // console.log(sessionStorage.getItem('pid'));
     var product_left = document.querySelector('.product .product-left');
     var phone_info = document.querySelector('.product .product-info .phone-info');
+    var pay_btn = document.querySelector('.btn-box .pay-btn');
+    var money = document.querySelector('.total .money span');
+    var phone_name = document.querySelectorAll('.phone-name');
+    var rel_price = document.querySelector('.rel-price');
+    // console.log(phone_name);
+
+    // 加入购物车操作
+    pay_btn.addEventListener('click', function () {
+        window.location.href = 'myCart.html';
+    });
+
 
     // Ajax连接接口
     const xhr = new XMLHttpRequest();
@@ -22,11 +33,15 @@ window.addEventListener("load", function () {
 
                     let h2 = document.createElement('h2');
                     h2.innerText = product.product_name;
+                    for (let i = 0; i < phone_name.length; i++) {
+                        phone_name[i].innerText = product.product_name;
+                    }
+
 
                     let sale_dec = document.createElement('p');
                     // let font = document.createElement('font');
                     sale_dec.classList.add('sale-dec');
-                    sale_dec.innerHTML = '<font>' + product.product_title + '</font>' + product.product_intro;
+                    sale_dec.innerHTML = '<font>' + product.product_title + '</font>&nbsp' + product.product_intro;
 
                     let ad = document.createElement('p');
                     ad.classList.add('ad');
@@ -40,12 +55,15 @@ window.addEventListener("load", function () {
                         let del = document.createElement('del');
                         del.innerText = product.product_price + '元';
                         price.appendChild(del);
+                        money.innerText = product.product_selling_price + '元';
+                        rel_price.innerHTML = `${product.product_selling_price}元&nbsp<del>${product.product_price}元</del>`
                     } else {
                         price.classList.add('price');
                         price.innerText = product.product_price + '元';
+                        money.innerText = product.product_price + '元';
+                        rel_price.innerHTML = `${product.product_selling_price}元`
                     }
-                    console.log(product_left);
-                    console.log(phone_info);
+                    
                     product_left.appendChild(img);
                     phone_info.appendChild(h2);
                     phone_info.appendChild(sale_dec);
@@ -60,12 +78,12 @@ window.addEventListener("load", function () {
         }
     }
 
-      // 登录后展示用户信息
-  var top_info = document.querySelector('.top-info');
-  const user = sessionStorage.getItem('user');
-  if (user != null && user != undefined) {
-    top_info.innerHTML = `Hi! ${user}
+    // 登录后展示用户信息
+    var top_info = document.querySelector('.top-info');
+    const user = sessionStorage.getItem('user');
+    if (user != null && user != undefined) {
+        top_info.innerHTML = `Hi! ${user}
        <span>|</span>
       <a href="#">消息通知</a>`
-  }
+    }
 })
