722fd1f810f33d3899d31e24f5a48bb73d0f2bae
“Waylon06” <205218290@qq.com>
2022-05-31 12:10:24 +0800

    22.5.31.1

---
+++ 图书管理系统/JS/index.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', function () {
+    var swiper = new Swiper(".mySwiper", {
+      speed:3000,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+      
+})
