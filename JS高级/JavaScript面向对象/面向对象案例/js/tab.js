35b55fd5ae136ae07e0ec12a125d9f77bdd33dc4
“Waylon06” <205218290@qq.com>
2022-07-05 13:48:56 +0800

    22.7.5.1

--- JS高级/JavaScript面向对象/面向对象案例/js/tab.js
+++ JS高级/JavaScript面向对象/面向对象案例/js/tab.js
@@ -1,29 +1,56 @@
+var that;
 class Tab {
     constructor(id) {
-        this.main =document.querySelector(id);
-        this.lis = this.main.querySelectorAll('li');
-        this.sections = this.main.querySelectorAll('section'); 
+        that = this;
+        this.main = document.querySelector(id);
+        this.add = this.main.querySelector('.tabadd');
+        // li的父元素
+        this.ul = this.main.querySelector('.firstnav ul:first-child');
+        // section 的父元素
+        this.fsection = this.main.querySelector('.tabscon');
         this.init();
     }
-    init(){
+    init() {
+        this.updateNode();
         // init初始化操作让让相关的元素绑定事件
+        this.add.onclick = this.addTab;
         for (let i = 0; i < this.lis.length; i++) {
             this.lis[i].index = i;
             this.lis[i].onclick = this.toggleTab;
         }
     }
+    // 获取所有的 li 和 section
+    updateNode() {
+        this.lis = this.main.querySelectorAll('li');
+        this.sections = this.main.querySelectorAll('section');
+    }
     // 1.切换功能
-    toggleTab(){
-        console.log(this.index);
-        this.className = ' liactive';
-        this.sections[this.index].className = 'conactive';
-        
-    }.
+    toggleTab() {
+        // console.log(this.index);
+        that.clearClass();
+        this.className = 'liactive';
+        that.sections[this.index].className = 'conactive';
+    }
+    clearClass() {
+        for (let i = 0; i < this.lis.length; i++) {
+            this.lis[i].className = '';
+            this.sections[i].className = '';
+        }
+    }
     // 2.添加功能
-    addTab(){}
+    addTab() {
+        that.clearClass();
+        // 1.创建li元素
+        var li = '<li class="liactive"><span>新选项卡</span><span class="iconfont icon-guanbi"></span></li>';
+        var section = '<section class="conactive">测试1</section>';
+        // 2.加到相应的父元素里面
+        that.ul.insertAdjacentHTML('beforeend', li);
+        that.fsection.insertAdjacentHTML('beforeend', section);
+        that.init();
+    }
     // 3.删除功能
-    removeTab(){}
+    removeTab() { }
     // 4.修改功能
-    editTab(){}
+    editTab() { }
 }
 new Tab('#tab');
