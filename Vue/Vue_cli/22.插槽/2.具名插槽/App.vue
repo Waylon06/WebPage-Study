946c07e82e9b7687f5740b507f69855c205b183b
“Waylon06” <205218290@qq.com>
2022-08-20 22:04:13 +0800

    22.8.20.2

---
+++ Vue/Vue_cli/22.插槽/2.具名插槽/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <Category title="美食" :listData="foods">
+      <img slot="center" src="./assets/2.webp" alt="" />
+    </Category>
+    <Category title="游戏" :listData="games">
+      <div slot="game" class="gamer">
+        <a href="#">单机游戏</a>
+        <a href="#">网络游戏</a>
+      </div>
+    </Category>
+    <Category title="电影" :listData="films">
+      <img slot="footer" src="./assets/6.webp" alt="" />
+    </Category>
+  </div>
+</template>
+
+<script>
+import Category from "./components/Category.vue";
+
+export default {
+  name: "App",
+  components: { Category },
+  data() {
+    return {
+      foods: ["火锅", "烧烤", "小龙虾"],
+      games: ["红色警戒", "穿越火线", "劲舞团"],
+      films: ["《教父》", "《拆弹专家》", "《你好，李焕英》"],
+    };
+  },
+};
+</script>
+
+<style>
+.container,
+.gamer {
+  display: flex;
+  justify-content: space-around;
+}
+
+h3 {
+  text-align: center;
+  background-color: orange;
+}
+</style>
--- Vue/Vue_cli/22.插槽/2.具名插槽/components/Category.vue
+++ Vue/Vue_cli/22.插槽/2.具名插槽/components/Category.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="category">
     <h3>{{ title }}</h3>
+    <ul>
+      <li v-for="(item, index) in listData" :key="index">{{ item }}</li>
+    </ul>
+    <!-- <img v-show="title === '美食'" src="../assets/2.webp" alt="" /> -->
     <!-- 定义一个插槽,等着组件的使用者进行填充 -->
-    <slot :games="games"></slot>
+    <slot name="center"></slot>
+    <slot name="game"></slot>
+    <slot name="footer"></slot>
   </div>
 </template>
 
@@ -10,11 +16,6 @@
 export default {
   name: "CategoryName",
   props: ["listData", "title"],
-  data() {
-    return {
-      games: ["红色警戒", "穿越火线", "劲舞团"],
-    };
-  },
 };
 </script>
 
@@ -26,6 +27,6 @@ export default {
 }
 
 img {
-  width: 100%;
+    width: 100%;
 }
 </style>
